a631ce9fac0af82066d2ed92047e8d8e154dc889
DarianaVega <145612689+DarianaVega@users.noreply.github.com>
2024-05-17 17:51:56 -0600

    seguimos en detalles

--- src/Containers/MainContainer.jsx
+++ src/Containers/MainContainer.jsx
@@ -1,18 +1,12 @@
 import React from "react";
 import "./mainContainer.css";
-import clara from "../images/clara.png"
 
 export default function MainContainer() {
   return (
     <>
       <div className="mainContainer">
         <div className="left-Container">
-          <p>Aprender de finanzas nunca fue tan fácil </p>
-        </div>
-        <div className="right-Container">
-          <div className="imageContainer">
-            <img className="clara" src={clara}></img>
-          </div>
+          <p><span class="color">Aprender</span> de <span class="color"><br></br>finanzas</span> nunca fue <br></br>tan <span class="color">fácil</span>l </p>
         </div>
       </div>
     </>
