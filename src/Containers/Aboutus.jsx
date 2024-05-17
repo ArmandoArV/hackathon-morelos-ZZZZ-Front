205e8dd190d6f090cc888ae063bbd6a6031eb968
DarianaVega <145612689+DarianaVega@users.noreply.github.com>
2024-05-17 17:20:35 -0600

    Detalles landing

--- src/Containers/Aboutus.jsx
+++ src/Containers/Aboutus.jsx
@@ -10,11 +10,8 @@ export default function Aboutus() {
         </div>
         <div className="text">
             <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text 
-            ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not 
-            only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s 
-            with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus 
-            PageMaker including versions of Lorem Ipsum.
+            Freya es tu nueva asesora en situaciones financieras, te ayudara a conocer mejor tus ingresos, egresos y deudas hasta te puede ayudar
+            dando mensajes para que aprendas a manejar mejor tu dinero. 
             </p>
         </div>
         </div>
--- src/Containers/Benefits.jsx
+++ src/Containers/Benefits.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import "./Benefits.css";
+import icono1 from "../images/icono1.png"
+import icono2 from "../images/icono2.png"
+import icono3 from "../images/icono3.png"
 
 export default function Benefits() {
   return (
@@ -11,28 +14,31 @@ export default function Benefits() {
         <div className="icon">
             <div className="primericon">
                 <div className="rectangulo1">
-                    <div className="circunferencia1">
-                        <div className="icon1">
-                            <p>icon</p>
-                        </div>
+                    <div className="icon1">
+                        <img className="icono1" src={icono1}></img>
+                    </div>
+                    <div className="texto1">
+                        <p>Decisiones financieras<br></br> informadas.</p>
                     </div>
                 </div> 
             </div>
             <div className="segundoicon">
                 <div className="rectangulo2">
-                    <div className="circunferencia2">
-                        <div className="icon2">
-                            <p>icon</p>
-                        </div>
+                    <div className="icon2">
+                        <img className="icono2" src={icono2}></img>
+                    </div>
+                    <div className="texto2">
+                        <p>Administración efectiva <br></br>de las finanzas.</p>
                     </div>
                 </div> 
             </div>
             <div className="tercericon">
                 <div className="rectangulo3">
-                    <div className="circunferencia3">
-                        <div className="icon3">
-                            <p>icon</p>
-                        </div>
+                    <div className="icon3">
+                        <img className="icono3" src={icono3}></img>
+                    </div>
+                    <div className="texto3">
+                        <p>Alcanzar metas financieras, <br></br>y contribuir al bienestar <br></br>de sus familias.</p>
                     </div>
                 </div> 
             </div>
