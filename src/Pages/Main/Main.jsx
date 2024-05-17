46d94868bbc8839b520b630fb00126e5707bce9d
DarianaVega <145612689+DarianaVega@users.noreply.github.com>
2024-05-17 06:07:24 -0600

    Pequeños avances

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import "./Main.css"
+import clara from "../../images/clara.png";
 
 export default function MainPage() {
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+    <main className="main">
+      <div className="landing">
+        <div className="left-container">
+          <p>Convierte</p>
+        </div>
+        <div className="right-container">
+          <img className="clara" src={clara}></img>
+        </div>
+      </div>
+    </main>
   );
 }
-
